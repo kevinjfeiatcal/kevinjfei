a316d2141002b05b39a1ce5741414f91b058ef4d
Kevin Fei <kevinjfei@berkeley.edu>
2025-09-15 15:51:18 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,59 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+    # Git is for mac, git bash runs the program git on pc, and github is online website to share/upload git projects
+# 2) What’s the difference between the terminal and the command line?
+
+# 3) How does Windows PowerShell differ from Git Bash?
+    # different language
+# 4) What’s the difference between Anaconda, conda, and Python?
+    # python is coding language
+    # conda is a package manager
+    # anaconda is something that comes with all the above
+# 5) What is VS Code? 
+    # a software that runs ... software
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+    # notebook: can .. run python?
+    # lab: better notebook?
+# 7) What does ~/ mean?
+    # home directory
+# 8) What’s the difference between an absolute path and a relative path?
+    # absolute is from the full path
+    # relative is a path relative to where the user currently is 
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+    # C:/Users/kevinjfei/python_decal_fs25/course_assignments/homework2
+    # course_assignments/homework2
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+    # cd .. 
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+    # remove the current directory
+# 12) What do the following commands do?
+# git add : adds the files to staging area
+# git commit : save the files in staging area
+# git push : pushes changes to online repository
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+    # . is all, file is js file
+# 14) What do "git status" and "git log -1" do?
+    # status opens status of whats happening
+    # log does most reent
+# 15) What’s the difference between cloning a repository and pulling from it?
+    # cloning makes new one, pulling updates existing one
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+    # getting the software to run in the first place LOL because it's difficult to understand
+    # but in terms of coding so far it's not bad
+# 17) What’s a question you still have? What’s something you’re confused about?
+    # n/a
+# 18) Tell me a fun fact!
+    # I'm recently beginning my first big embroidery project! making snoopy and woodstock
+    # on my converse. taking many hours more than I'd like, and now I'm just unable to 
+    # wear my shoes probably for at least another month or two...
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(60+7) # man i dont know
